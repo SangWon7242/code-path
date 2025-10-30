289c7e5aa1cfdac96c096d1e3d9d3eda49606acc
SangWon7242 <gotkdqja@gmail.com>
2025-10-30 09:07:45 +0900

    코드 구조 변경

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,16 @@
 import Header from "../components/layout/Header";
+import MainSection from "./components/MainSection";
+import QuestionSection from "./components/QuestionSection";
 
 // { children: React.ReactNode } : children이라는 프롭(prop)의 타입이 React.ReactNode임을 명시
 export default function App({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <main>
+        <MainSection />
+        <QuestionSection />
+      </main>
     </>
   );
 }
